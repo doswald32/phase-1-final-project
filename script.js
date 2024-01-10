cc49d873e273ccc7074b201d76a8f4f2853e785c
doswald32 <dan.oswald32@gmail.com>
2024-01-09 23:14:52 -0700

    each checkbox now has its own unique ID. Need to create event listeners for each

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function renderPlayers(player, num){
+function renderPlayers(team, player, num){
     //build player card
     let card = document.createElement('li');
     card.className = 'card';
@@ -6,11 +6,11 @@ function renderPlayers(player, num){
     card.innerHTML = `
     <div class="card-container borders">
         <img id="player-image" src="${player.imageURL}">
-        <form action="http://www.example.com/profile.php">
-            <input id="player-select-checkbox" type="checkbox" name="player-select" value="player-select" />
+        <form id="checkbox" action="http://www.example.com/profile.php">
+            <input id="${team}-player-select-checkbox-${player.id}" class="player-select-checkbox" type="checkbox" name="player-select" value="player-select" />
         </form>
         <div class="card-content">
-            <p id="player-name">${player.name}, ${player.position}, Age: ${player.age}</p>
+            <p>${player.name}, ${player.position}, Age: ${player.age}</p>
             <p>${player.ppg}ppg, ${player.apg}apg, ${player.rpg}rpg, Salary: $${player.salary}M</p>
         </div>
     </div>
@@ -23,7 +23,7 @@ function renderPlayers(player, num){
 function getTeamPlayers(team, num) {
     fetch(`http://127.0.0.1:3000/${team}`)
     .then(res => res.json())
-    .then(teamData => teamData.forEach(player => renderPlayers(player, num)))
+    .then(teamData => teamData.forEach(player => renderPlayers(team, player, num)))
 };
 
 function getTeamStats(team, num) {
@@ -78,3 +78,14 @@ teamSelectorTwo.addEventListener("change", function(e, team, num = 2){
     playerWindow.innerHTML = '';
     getTeamStats(team, num);
 });
+
+function checkboxListener(team) {
+    for (let i = 1; i <= 15; i++) {
+        document.getElementById(`${team}-player-select-checkbox-${i}`).addEventListener('click', () => console.log('test'))
+    }
+};
+
+checkboxListener('Bulls');
+
+// let card = document.getElementById('player-select-checkbox');
+// card.addEventListener('click', () => console.log('testing'));
